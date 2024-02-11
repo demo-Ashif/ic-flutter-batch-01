60e677eb582a8b502687173f4c15d3c0b6640345
Ashif Mujtoba <nmujtoba@gmail.com>
2024-02-11 22:29:58 +0600

    commit after live class 15

--- ic_flutter_batch_first_app/lib/main.dart
+++ ic_flutter_batch_first_app/lib/main.dart
@@ -1,142 +1,191 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyTextInput());
-  // runApp(MyApp());
-  // runApp(MyButton());
+  // runApp(GestureWidget());
+  // runApp(ListWidget());
+  runApp(DynamicListWidget());
 }
 
-class MyButton extends StatelessWidget {
+class GestureWidget extends StatelessWidget {
+  const GestureWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Image.asset(
-            'images/superman.jpg',
-            fit: BoxFit.cover,
-          ),
+        body: Column(
+          children: [
+            SizedBox(height: 50),
+            GestureDetector(
+              onTap: () {
+                print('On Tap Happened!');
+              },
+              onDoubleTap: () {
+                print('On DoubleTap Happened!');
+              },
+              onLongPress: () {
+                print('On LongPressed Happened!');
+              },
+              child: Container(
+                height: 150,
+                width: 150,
+                color: Colors.redAccent,
+                child: Text('My Container'),
+              ),
+            ),
+            SizedBox(height: 50),
+            InkWell(
+              onLongPress: () {
+                print('On LongPressed Happened!');
+              },
+              splashColor: Colors.amber,
+              radius: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text('Click Me!'),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-class MyApp extends StatelessWidget {
+// class ListWidget extends StatelessWidget {
+//   ListWidget({super.key});
+//
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     List<String> sampleNames = [
+//       'Alice', 'Bob', 'Charlie', 'Diana', 'Evan',
+//       'Fiona', 'George', 'Hannah', 'Ian', 'Jenna',
+//       'Kyle', 'Luna', 'Mike', 'Nora', 'Oliver'
+//     ];
+//
+//     List<Student> students = List.generate(sampleNames.length, (index) {
+//       return Student(name: sampleNames[index], roll: index + 1);
+//     });
+//
+//
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: true,
+//       home: SafeArea(
+//         child: Scaffold(
+//           body: ListView(
+//             children: [
+//               ListTile(
+//                 onTap: () {
+//                   print('On tap student!');
+//                 },
+//                 leading: Icon(Icons.abc),
+//                 title: Text('Student Name: ${studentNames[0]}'),
+//                 subtitle: Text('Student Roll: 42'),
+//                 style: ListTileStyle.list,
+//               ),
+//               ListTile(
+//                 onTap: () {
+//                   print('On tap student!');
+//                 },
+//                 leading: Icon(Icons.abc),
+//                 title: Text('Student Name: ${studentNames[1]}'),
+//                 subtitle: Text('Student Roll: 43'),
+//                 style: ListTileStyle.list,
+//               ),
+//               ListTile(
+//                 onTap: () {
+//                   print('On tap student!');
+//                 },
+//                 leading: Icon(Icons.abc),
+//                 title: Text('Student Name: ${studentNames[2]}'),
+//                 subtitle: Text('Student Roll: 32'),
+//                 style: ListTileStyle.list,
+//               ),
+//               ListTile(
+//                 onTap: () {
+//                   print('On tap student!');
+//                 },
+//                 leading: Icon(Icons.abc),
+//                 title: Text('Student Name: ${studentNames[3]}'),
+//                 subtitle: Text('Student Roll: 67'),
+//                 style: ListTileStyle.list,
+//               ),
+//               ListTile(
+//                 onTap: () {
+//                   print('On tap student!');
+//                 },
+//                 leading: Icon(Icons.abc),
+//                 title: Text('Student Name: ${studentNames[4]}'),
+//                 subtitle: Text('Student Roll: 72'),
+//                 style: ListTileStyle.list,
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class DynamicListWidget extends StatelessWidget {
+  DynamicListWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
+    List<String> sampleNames = [
+      'Alice',
+      'Bob',
+      'Charlie',
+      'Diana',
+      'Evan',
+      'Fiona',
+      'George',
+      'Hannah',
+      'Ian',
+      'Jenna',
+      'Kyle',
+      'Luna',
+      'Mike',
+      'Nora',
+      'Oliver'
+    ];
+
+    List<Student> students = List.generate(sampleNames.length, (index) {
+      return Student(name: sampleNames[index], roll: index + 1);
+    });
+
     return MaterialApp(
-      title: 'Flutter First App',
       debugShowCheckedModeBanner: true,
       home: SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.black87,
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color: Colors.blue,
-                width: 120,
-                height: 120,
-              ),
-              Container(
-                color: Colors.amber,
-                width: 120,
-                height: 120,
-              ),
-              Container(
-                color: Colors.redAccent,
-                width: 120,
-                height: 120,
-              ),
-            ],
-          ),
+          body: ListView.builder(
+              itemCount: students.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                          'Student: ${students[index].name} & Roll: ${students[index].roll}'),
+                    ));
+                  },
+                  leading: Icon(Icons.abc),
+                  title: Text('Student Name: ${students[index].name}'),
+                  subtitle: Text('Student Roll: ${students[index].roll}'),
+                  style: ListTileStyle.list,
+                );
+              }),
         ),
       ),
     );
   }
-
-// Row(
-// mainAxisAlignment: MainAxisAlignment.spaceAround,
-// children: [
-// Container(
-// color: Colors.blue,
-// width: 70,
-// height: 120,
-// ),
-// Container(
-// color: Colors.amber,
-// width: 70,
-// height: 120,
-// ),
-// Container(
-// color: Colors.redAccent,
-// width: 70,
-// height: 120,
-// )
-// ],
-// )
 }
 
-class MyTextInput extends StatelessWidget {
-  MyTextInput({super.key});
+class Student {
+  String name;
+  int roll;
 
-  TextEditingController _controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: TextField(
-              controller: _controller,
-              enabled: true,
-              onTap: () {
-                print('Input field tapped!');
-              },
-              onChanged: (String text) {
-                print('$text');
-              },
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.search,
-              onSubmitted: (String value) {
-                _controller.clear();
-                _controller.text = 'New Search';
-              },
-              obscureText: false,
-              decoration: InputDecoration(
-                // suffixIcon: Icon(Icons.add),
-                // prefixIcon: Icon(Icons.abc_rounded),
-                label: Text('UserName'),
-                hintText: 'Please give username',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.blue,
-                    width: 2,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.redAccent,
-                    width: 2,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.amber,
-                    width: 2,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  Student({required this.name, required this.roll});
 }
