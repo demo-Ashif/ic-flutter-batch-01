ca47917790ea3a5cc74c25510cae38e116b46df8
Ashif Mujtoba <ashif.mujtoba@mpower-social.com>
2024-02-20 20:30:19 +0600

    added theme data branch and demo ui code

--- ic_flutter_batch_first_app/lib/main.dart
+++ ic_flutter_batch_first_app/lib/main.dart
@@ -1,19 +1,3 @@
-import 'package:flutter/material.dart';
-
-import 'home_screen.dart';
-
 void main() {
-  runApp(NavigationLearning());
-}
-
-class NavigationLearning extends StatelessWidget {
-  const NavigationLearning({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: HomeScreen(),
-    );
-  }
+  //runApp(MyApp());
 }
