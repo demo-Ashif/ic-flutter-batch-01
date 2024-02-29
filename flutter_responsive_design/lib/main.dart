082874bf481f5b4cc48c905d520677ce7199d8af
Ashif Mujtoba <nmujtoba@gmail.com>
2024-03-01 02:57:36 +0600

    commit after flutter class 08

--- flutter_responsive_design/lib/main.dart
+++ flutter_responsive_design/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,7 +23,109 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blueGrey,
         ),
-        body: SingleChildScrollView(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Flexible Widget', style: TextStyle(fontSize: 24)),
+              Row(
+                children: <Widget>[
+                  Flexible(
+                      flex: 2,
+                      child: Container(color: Colors.blue, height: 100)),
+                  Flexible(
+                      flex: 1,
+                      child: Container(color: Colors.red, height: 100)),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text('Expanded Widget', style: TextStyle(fontSize: 24)),
+              Row(
+                children: <Widget>[
+                  Expanded(child: Container(color: Colors.purple, height: 100)),
+                  Container(color: Colors.orange, width: 50, height: 100),
+                ],
+              ),
+              SizedBox(height: 20),
+              Text('AspectRatio Widget', style: TextStyle(fontSize: 24)),
+              AspectRatio(
+                  aspectRatio: 3 / 2, child: Container(color: Colors.green)),
+              SizedBox(height: 20),
+              Text('FittedBox Widget', style: TextStyle(fontSize: 24)),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.orange,
+                child: FittedBox(
+                  child: Text(
+                    'Long Text',
+                    maxLines: 1,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text('SizedBox Widget', style: TextStyle(fontSize: 24)),
+              SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Container(color: Colors.orange)),
+              SizedBox(height: 20),
+              Text('FractionallySizedBox Widget',
+                  style: TextStyle(fontSize: 24)),
+              FractionallySizedBox(
+                  widthFactor: 0.5,
+                  child: Container(color: Colors.yellow, height: 100)),
+              SizedBox(height: 20),
+              Text('Wrap Widget', style: TextStyle(fontSize: 24)),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 100,
+                    color: Colors.green,
+                    margin: EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 100,
+                    color: Colors.amber,
+                    margin: EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 100,
+                    color: Colors.red,
+                    margin: EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 30,
+                    color: Colors.orange,
+                    margin: EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.green,
+                    margin: EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 100,
+                    color: Colors.blue,
+                    margin: EdgeInsets.all(8),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 100,
+                    color: Colors.orange,
+                    margin: EdgeInsets.all(8),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
